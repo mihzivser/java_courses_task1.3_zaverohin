7e086290d2063d3f6a1d5748ceb07d76a7ecc490
mihzivser <mihzivser@gmail.com>
2026-03-01 22:28:16 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -9,7 +9,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-
+    // RESURS
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres"; // ← свой пароль
